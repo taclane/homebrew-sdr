def70303fe11e0e23bb21ac36d873c1825ef6833
Nick M <taclane@users.noreply.github.com>
2023-12-15 23:23:04 -0500

    add stable

--- Formula/trunk-recorder.rb
+++ Formula/trunk-recorder.rb
@@ -1,10 +1,13 @@
 class TrunkRecorder < Formula
   desc "Trunked & Conventional Radio Recorder"
   homepage "https://github.com/robotastic/trunk-recorder"
-  url "https://github.com/robotastic/trunk-recorder/archive/refs/tags/v4.6.0.tar.gz"
-  sha256 "01c85bd8504c19c138f52a7e1ba7cfabd6e539b0d202b4ed4d6f26ed8b61935f"
   license "GPL-3.0-or-later"
 
+  stable do
+    url "https://github.com/robotastic/trunk-recorder/archive/refs/tags/v4.6.0.tar.gz"
+    sha256 "01c85bd8504c19c138f52a7e1ba7cfabd6e539b0d202b4ed4d6f26ed8b61935f"
+  end
+  
   head do
     url "https://github.com/robotastic/trunk-recorder.git", branch: "master"
   end
