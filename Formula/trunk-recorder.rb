a01e673261fae4d1be2cb1c29097e80272b2af4b
Nick M <taclane@users.noreply.github.com>
2024-09-04 12:05:20 -0400

    f4

--- Formula/trunk-recorder.rb
+++ Formula/trunk-recorder.rb
@@ -33,8 +33,6 @@ class TrunkRecorder < Formula
   end
 
   test do
-  # Start Trunk Trecorder with an empty config and interrupt after a short
-  #   timeout if no fatal errors encountered running/ending the flowgraph
     (testpath/"test.json").write <<~EOS
       {
         "ver": 2,
