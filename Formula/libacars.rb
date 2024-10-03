ef4a7f84033e0f915951814cbb22b14086f2a5ab
Nick M <taclane@users.noreply.github.com>
2024-10-03 17:13:33 -0400

    libacars

--- Formula/libacars.rb
+++ Formula/libacars.rb
@@ -4,15 +4,12 @@ class Libacars < Formula
   url "https://github.com/szpajder/libacars/archive/refs/tags/v2.2.0.tar.gz"
   sha256 "495e9836c8a1033a5d7814e68ebfc3b0d8f7db1747d13310e7df47c561aee9ce"
   license "MIT"
-
-  head do
-    url "https://github.com/szpajder/libacars.git", branch: "master"
-  end
+  head "https://github.com/szpajder/libacars.git", branch: "master"
 
   depends_on "cmake" => :build
   depends_on "zlib"
-  depends_on "libxml2"
   depends_on "jansson"
+  depends_on "libxml2"
 
   def install
     system "cmake", "-B", "build", *std_cmake_args
