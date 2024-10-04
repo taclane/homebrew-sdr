6f507b98674c79dcb3bf855d6f42d4ee1395240c
Nick M <taclane@users.noreply.github.com>
2024-10-04 09:24:15 -0400

    march onward

--- Formula/acarsdec.rb
+++ Formula/acarsdec.rb
@@ -1,14 +1,14 @@
 class Acarsdec < Formula
   desc "Multi-channels ACARS decoder"
-  #homepage "https://github.com/TLeconte/acarsdec"
+  # homepage "https://github.com/TLeconte/acarsdec"
   homepage "https://github.com/fredclausen/acarsdec"
   url "https://github.com/TLeconte/acarsdec/archive/refs/tags/acarsdec-3.7.tar.gz"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   license "GPL-2.0"
 
   # head "https://github.com/TLeconte/acarsdec.git", branch: "master"
-  head "https://github.com/fredclausen/acarsdec.git", branch: "master" 
-  
+  head "https://github.com/fredclausen/acarsdec.git", branch: "master"
+
   depends_on "cmake" => :build
   depends_on "cjson"
   depends_on "libacars"
@@ -16,13 +16,13 @@ class Acarsdec < Formula
   depends_on "libsndfile"
   depends_on "libusb"
   depends_on "paho-mqtt-c"
-  depends_on "soapysdr"
   depends_on "soapyrtlsdr"
+  depends_on "soapysdr"
 
   def install
     args = %W[
       -Bbuild
-      -DCMAKE_C_FLAGS="-march=native"
+      -DCMAKE_C_FLAGS=-march=native
     ]
     system "cmake", *std_cmake_args, *args
     system "cmake", "--build", "build"
