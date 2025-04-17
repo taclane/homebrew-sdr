abf2c2a64199c55e7cfe0eeee845aaca341964ef
Nick M <taclane@users.noreply.github.com>
2025-04-17 16:47:27 -0400

    formula audit corrections

--- Formula/trunk-recorder.rb
+++ Formula/trunk-recorder.rb
@@ -1,8 +1,6 @@
 class TrunkRecorder < Formula
   desc "Trunked & Conventional Radio Recorder"
   homepage "https://github.com/robotastic/trunk-recorder"
-  url "https://github.com/trunkrecorder/trunk-recorder/archive/refs/tags/v5.0.1.tar.gz"
-  sha256 "1c9a8a625506c5b5cc14948404e70b86333ea9cd08c0cfef117f611497d5b2be"
   license "GPL-3.0-or-later"
   head "https://github.com/robotastic/trunk-recorder.git", branch: "master"
 
@@ -16,16 +14,20 @@ class TrunkRecorder < Formula
   depends_on "fmt"
   depends_on "gmp"
   depends_on "gnuradio"
-  depends_on "taclane/sdr/gr-osmosdr"
   depends_on "openssl@3"
   depends_on "sox"
   depends_on "spdlog"
+  depends_on "taclane/sdr/gr-osmosdr"
   depends_on "uhd"
   depends_on "volk"
 
   stable do
+    url "https://github.com/trunkrecorder/trunk-recorder/archive/refs/tags/v5.0.1.tar.gz"
+    sha256 "1c9a8a625506c5b5cc14948404e70b86333ea9cd08c0cfef117f611497d5b2be"
+    # A patch is required to build with updated Boost libraries provided by Homebrew.
+    # Remove on next tagged release; these fixes are already present in the master branch.
     patch do
-      url "https://raw.githubusercontent.com/taclane/homebrew-sdr/refs/heads/main/Patches/trunk-recorder-5.0.1.diff"
+      url "https://raw.githubusercontent.com/taclane/homebrew-sdr/f531ac261373c557ba7b2312b79e3708f90adb2b/Patches/trunk-recorder-5.0.1.diff"
       sha256 "db20b1e0ac0534bc4fd189be93b6cf67d6bdd188ba8907e49c960760801df586"
     end
   end
