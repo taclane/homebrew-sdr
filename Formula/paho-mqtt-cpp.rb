75ec9a5df47eba02f0367a260a8859f2b0f98db6
Nick M <taclane@users.noreply.github.com>
2025-07-08 11:30:55 -0400

    update paho cpp

--- Formula/paho-mqtt-cpp.rb
+++ Formula/paho-mqtt-cpp.rb
@@ -1,8 +1,8 @@
 class PahoMqttCpp < Formula
   desc "Eclipse Paho C++ Client Library for the MQTT Protocol"
   homepage "https://github.com/eclipse/paho.mqtt.cpp"
-  url "https://github.com/eclipse/paho.mqtt.cpp/archive/refs/tags/v1.4.1.tar.gz"
-  sha256 "48e7ba6e1032aa73e4d985a7387e02a77cc5807a8420d16790b84b941d86374e"
+  url "https://github.com/eclipse-paho/paho.mqtt.cpp/archive/refs/tags/v1.5.2.tar.gz"
+  sha256 "3d8d9bfee614d74fa2e28dc244733c79e4868fa32a2d49af303ec176ccc55bfb"
   license "EPL-2.0"
   head "https://github.com/eclipse/paho.mqtt.cpp.git", branch: "master"
 
